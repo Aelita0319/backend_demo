f7515caeb43f7129420771a6a4109f0587b3262f
Aelita <386339484@qq.com>
2021-06-10 11:47:01 +0800

    add account

--- src/main/java/com/example/mybatisdemo/controller/AccountController.java
+++ src/main/java/com/example/mybatisdemo/controller/AccountController.java
@@ -1,7 +1,11 @@
 package com.example.mybatisdemo.controller;
 
 import com.example.mybatisdemo.model.Account;
+import com.example.mybatisdemo.model.Bank;
+import com.example.mybatisdemo.model.User;
 import com.example.mybatisdemo.repository.AccountRepository;
+import com.example.mybatisdemo.repository.BankRepository;
+import com.example.mybatisdemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +16,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(path="/account")
 public class AccountController {
     @Autowired
-
     private AccountRepository accountRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BankRepository bankRepository;
 
     @RequestMapping(value = "/add")
     public @ResponseBody Integer addAccount(Account account){
+        //0:user, 1:admin, 2:bank
         Account result = accountRepository.save(account);
+        if (result.getPrivilege()==0){
+            User user = new User();
+            user.setCompanyName(account.getName());
+            userRepository.save(user);
+        }
+        else if (result.getPrivilege()==2){
+            Bank bank = new Bank();
+            bank.setName(account.getName());
+            bank.setInterest(0.01);
+            bank.setArg1((double) 0);
+            bank.setArg2((double) 0);
+            bank.setArg3((double) 0);
+            bankRepository.save(bank);
+        }
         return result.getId();
     }
 
